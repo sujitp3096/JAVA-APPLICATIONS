246eed60aa0249e0aae4ed5671463a37b49243d9
Sujit Pawar <sujitspawar9696@gmail.com>
2025-11-18 23:48:22 +0530

    Create ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server started. Waiting for client...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected!");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                String clientMsg = in.readLine();
+                System.out.println("Client: " + clientMsg);
+
+                System.out.print("You: ");
+                String reply = keyboard.readLine();
+                out.println(reply);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
